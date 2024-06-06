4bd3cedc0fd4d3d4eed955d7ba1f64b6cd49e603
BakangMonei <bakangmonei2@gmail.com>
2024-06-06 15:43:11 +0200

    awesome

---
+++ src/backend/checkENVFile.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+
+
+console.log('Checking .env file...');
+console.log('PORT: ', process.env.PORT);
+console.log('FIREBASE_API_KEY: ', process.env.FIREBASE_API_KEY);
--- src/backend/server.js
+++ src/backend/server.js
@@ -1,5 +1,8 @@
 const app = require('./app');
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+const port = process.env.PORT;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
